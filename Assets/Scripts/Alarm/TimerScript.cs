1919eecc27d150f6616a937fa9d66c8a9bb9b823
Markus Schuetz <markus.schuetz@student.tugraz.at>
2018-02-23 13:07:55 +0100

    Commit for merge, alarm partwise done

---
+++ Assets/Scripts/Alarm/TimerScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+    public int seconds;
+    private Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText = gameObject.GetComponent<Text>();
+        timerText.text = seconds.ToString();
+        InvokeRepeating("UpdateTimer", 1.0f, 1.0f);
+	}
+
+    void UpdateTimer()
+    {
+        int sec = Int32.Parse(timerText.text);
+        if(sec <= 0)
+        {
+            Debug.Log("Time ran out.");
+            CancelInvoke("UpdateTimer");
+            timerText.text = "0";
+            return;
+        }
+
+        timerText.text = (sec - 1).ToString();
+
+    }
+
+
+	
+	
+
+}
